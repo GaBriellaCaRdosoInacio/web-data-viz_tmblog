c4ea2cd8429ff126c3a5d66bc7e76115c2fb6733
gabriella <gabriella.inacio@sptech.school>
2023-05-25 21:07:38 -0300

    teste pontuacao quiz

--- web-data-viz/web-data-viz/site/public/js/pergunta-quiz.js
+++ web-data-viz/web-data-viz/site/public/js/pergunta-quiz.js
@@ -1,9 +1,7 @@
 // pergunta.innerHTML= listaPerg[contadorP];
 var listaPerg= [["1- Você considera sua imaginação fértil?"],["2- Você se considera uma pessoa que atrai sorte?"], ["3- Se um amigo seu está com problemas, você tentaria diversas coisas para poder ajuda-lo?"], ["4- Você tenta ser positivo mediante as dificuldades?"], ["5- Sua família é caótica?"]];
 var contadorP=0;
-var listaResp=[];
-var listaGabarito= [' ', 'A', 'B', 'A','A', 'A'];
-var acerto=0;
+
 
 function proximo(){
     if(contadorP< listaPerg.length){
--- web-data-viz/web-data-viz/site/public/js/validacao.js
+++ web-data-viz/web-data-viz/site/public/js/validacao.js
@@ -1,17 +1,34 @@
 
 //var global validar resposta
 var listaResp= [];
+var contadorQuiz=0;
+var listaGabarito= [' ', 'A', 'B', 'A','A', 'A'];
+var acerto=0;
 
 function validar() {
 
-    var select = opcoes.value;
-    listaResp.push(select);
+    contadorQuiz++
 
-    for(var posicao=0; posicao<listaResp.lenght; posicao++){
-        if(listaResp[posicao] == listaGabarito[posicao]){
-            acerto+=1;
-        }
-    } 
+    if(contadorQuiz>0){
+
+    if(contadorQuiz==1){
+        var select = opcoes.value;
+        listaResp.push(select-1);
+    }
+
+    else{
+        alert('Só pode validar a resposta uma vez');
+    }
+    
+
+    // for(var posicao=0; posicao<listaResp.lenght; posicao++){
+    //     if(listaResp[posicao] == listaGabarito[posicao]){
+    //         acerto+=1;
+    //     }
+    // } 
+
+        
+}
     alert(`${acerto}`)
 
 }
