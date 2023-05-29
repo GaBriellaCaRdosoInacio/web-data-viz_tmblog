25fbd99ef96e2a9d91c524b9834a2487af3fd8c0
GaBriellaCaRdosoInacio <gabriella.inacio@sptech.school>
2023-05-29 11:03:37 -0300

    mudei o script da API

--- web-data-viz/web-data-viz/site/src/database/script-tabelas.sql
+++ web-data-viz/web-data-viz/site/src/database/script-tabelas.sql
@@ -6,44 +6,57 @@
 comandos para mysql - banco local - ambiente de desenvolvimento
 */
 
-CREATE DATABASE aquatech;
+create database tmblog;
 
-USE aquatech;
+use tmblog;
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+create table usuario(
+iduser int primary key auto_increment,
+nome varchar(45), 
+email varchar(50),
+senha char(8) 
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table quiz_pergunta(
+idpergnt int primary key auto_increment,
+pergunta int
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
+create table quiz_resposta(
+idresp int primary key auto_increment,
+resposta char(1)
 );
+
+create table resultado_quiz(
+fkpergunta int,
+constraint fkpergunta foreign key (fkpergunta)
+references quiz_pergunta (idpergnt),
+fkresposta int,
+constraint fkresposta foreign key (fkresposta)
+references quiz_resposta (idresp),
+constraint pkcomposta primary key (fkpergunta,fkresposta),
+fkuser int,
+constraint fkuser foreign key (fkuser)
+references usuario (iduser),
+porcento_xaveco int
+);
+
+
+
 
 /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+-- create table medida (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	dht11_umidade DECIMAL,
+-- 	dht11_temperatura DECIMAL,
+-- 	luminosidade DECIMAL,
+-- 	lm35_temperatura DECIMAL,
+-- 	chave TINYINT,
+-- 	momento DATETIME,
+-- 	fk_aquario INT,
+-- 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+-- );
 
 
 /*
