cade61cc448f370d26a8ad43a5ff7fa2faaae47a
gabriella <gabriella.inacio@sptech>
2023-06-07 14:39:23 -0300

    quiz sem validação mas pegando

--- web-data-viz/web-data-viz/site/public/js/pergunta.js
+++ web-data-viz/web-data-viz/site/public/js/pergunta.js
@@ -3,12 +3,13 @@
 // pergunta.innerHTML= listaPerg[contadorP];
 var listaPerg= [["1- Você considera sua imaginação fértil?"],["2- Você se considera uma pessoa que atrai sorte?"], ["3- Se um amigo seu está com problemas, você tentaria diversas coisas para poder ajuda-lo?"], ["4- Você tenta ser positivo mediante as dificuldades?"], ["5- Sua família é caótica?"]];
 var contadorP=0;
-
+var proximaQ=0;
 
 function proximo(){
     if(contadorP< listaPerg.length){
     pergunta.innerHTML= listaPerg[contadorP];
 }
+proximaQ+=1;
 
 oi.innerHTML= ' ';
 clique.innerHTML= ' ';
